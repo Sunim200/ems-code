b90fa293d805bf59ad98ad571a16842f8afa3e42
emin <emin@duweisst.de>
2023-11-09 19:07:34 +0100

    [feature-add]: index site, setup

---
+++ main.js
@@ -0,0 +1,7 @@
+const musik = document.getElementById('musik');
+const dev = document.getElementById('dev');
+const goku = document.getElementById('goku');
+
+musik.addEventListener('click', () => {
+    window.open("index.html", "_blank");
+})
